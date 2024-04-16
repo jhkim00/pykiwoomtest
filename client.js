e08bbfa3f48b0c783d385d7fc69e07954ee6aabe
JinHongKim <jhkimcoder@gmail.com>
2024-04-17 02:21:13 +0900

    차트 그리는 코드 수정. 일단 그려지긴 함.

--- client.js
+++ client.js
@@ -8,14 +8,6 @@ socket.addEventListener('open', function (event) {
 
 // 캔들스틱 데이터와 이동평균 데이터를 저장할 배열 초기화
 var candlestickData = [];
-var movingAverageData = {
-    x: [],
-    y: [],
-    type: 'scatter',
-    mode: 'lines',
-    line: { color: 'blue' },
-    name: 'Moving Average'
-};
 
 // 웹소켓 메시지 수신 시
 socket.addEventListener('message', function (event) {
@@ -25,11 +17,11 @@ socket.addEventListener('message', function (event) {
     // 캔들스틱 데이터를 생성하고 저장
     dataArray.forEach(function(data) {
         var candlestickDatum = {
-            x: data['date'],
-            open: parseFloat(data['open']),
-            high: parseFloat(data['high']),
-            low: parseFloat(data['low']),
-            close: parseFloat(data['close']),
+            x: [data['date'].replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3')],
+            open: [parseFloat(data['open'])], // 시가를 배열로 전달
+            high: [parseFloat(data['high'])], // 고가를 배열로 전달
+            low: [parseFloat(data['low'])], // 저가를 배열로 전달
+            close: [parseFloat(data['close'])], // 종가를 배열로 전달
             type: 'candlestick',
             increasing: { line: { color: 'green' } },
             decreasing: { line: { color: 'red' } },
@@ -37,27 +29,12 @@ socket.addEventListener('message', function (event) {
         };
         candlestickData.push(candlestickDatum);
 
-        console.log('일자:' + data['date']);
+        console.log('일자:' + data['date'].replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'));
         console.log('시가:' + data['open']);
         console.log('고가:' + data['high']);
         console.log('저가:' + data['low']);
         console.log('현재가:' + data['close']);
-
-        // 이동평균 데이터 추가
-        movingAverageData.x.push(data['date']);
-        movingAverageData.y.push(parseFloat(data['close']));
-        if (movingAverageData.y.length >= 20) {
-            var sum = 0;
-            for (var i = movingAverageData.y.length - 20; i < movingAverageData.y.length; i++) {
-                sum += movingAverageData.y[i];
-            }
-            var average = sum / 20;
-            movingAverageData.y.push(average);
-        }
     });
 
-    // 캔들차트에 이동평균선 추가하여 그리기
-    var dataToPlot = candlestickData.slice();
-    dataToPlot.push(movingAverageData);
-    Plotly.newPlot('graph', dataToPlot);
+    Plotly.newPlot('graph', candlestickData);
 });
